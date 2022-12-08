5b4c32b19368804d9b4f7e48c4e2130c10e5e6c0
prts <429558984@qq.com>
2022-12-08 14:28:42 +0800

    在原有基础上添加了cloud依赖，当前新增注册中心nacos

--- gulimall-coupon/src/main/java/com/zf/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/zf/gulimall/coupon/GulimallCouponApplication.java
@@ -4,9 +4,11 @@ package com.zf.gulimall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.zf.gulimall.coupon.dao")
+@EnableDiscoveryClient
 public class GulimallCouponApplication {
 
     public static void main(String[] args) {
--- gulimall-product/src/main/java/com/zf/common/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/zf/common/product/GulimallProductApplication.java
@@ -3,8 +3,10 @@ package com.zf.common.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.zf.common.product.dao")
 public class GulimallProductApplication {
 
